54c4d197abf530b219e4009b01efbde4308a9274
Glitch (grave-dormouse) <none>
2018-11-25 09:52:02 +0000

    😽🐕 Checkpoint ./server.js:2018030/16

--- server.js
+++ server.js
@@ -68,8 +68,8 @@ app.post('/api/exercise/add',(req,res)=>{
 
 // 3. GET /api/exercise/log?{userId}[&from][&to][&limit]
 app.get('/api/exercise/log',(req,res)=>{
-  p('id: ',req.query);
-  res.json({userId:''},(e,d)=>{
+  p('id: ');p(req.query);
+  res.json({userId:req.query},(e,d)=>{
     e?p(e):p(d);
   });
 });
