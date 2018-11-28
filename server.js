6cac3e7e3c802882672548c291d0fd20b238f224
Glitch (grave-dormouse) <none>
2018-11-28 05:26:41 +0000

    📒🎸 Checkpoint ./server.js:2018030/456

--- server.js
+++ server.js
@@ -127,12 +127,12 @@ app.post('/api/exercise/add',(req,res)=>{
     }
     //user found:
     else{
-      //init date
-      var date = new moment();
-      p(date.match(/\d{4}-\d{1,2}-\d{1,2}/g));
+      
       //if date not present  
       if(req.body.date.length==0){
-        
+        //init date
+        var date = moment().year().toString()+'-'+moment().month().toString()+'-'+moment().date().toString();
+        p(date);
         
         return res.send("Acknowledged",(e,d)=>{
           e?p(e):p(d);
@@ -145,7 +145,7 @@ app.post('/api/exercise/add',(req,res)=>{
         oldUser.data.push({date:req.body.date,desc:req.body.description,dur:req.body.duration});
         oldUser.save((e,d)=>{  
           if(e)return p(e);
-          res.send(oldUser,(e,d)=>{
+          return res.send(oldUser,(e,d)=>{
             e?p(e):p(d);
           });
         });
